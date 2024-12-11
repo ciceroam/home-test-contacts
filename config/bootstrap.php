9c3b2933129d9c06e100ba6666a759477a4ec0ea
Cicero Melo <ciceroam@gmail.com>
2024-12-11 12:11:44 -0300

    feat: Initial core implementation of the Contacts-App
    - Added core backend functionality with Symfony 4.4.
    - Implemented CRUD operations for contacts.
    - Integrated MySQL database with Doctrine ORM.
    - Added user authentication and role-based access control.

---
+++ config/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (!class_exists(Dotenv::class)) {
+    throw new LogicException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+}
+
+if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
+    (new Dotenv(false))->populate($env);
+} else {
+    (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
+}
+
+if (!class_exists(AnnotationReader::class)) {
+    throw new \RuntimeException('Doctrine Annotations not properly installed');
+}
+
+$_SERVER += $_ENV;
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+return new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
